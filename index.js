e937b6cf243c2b21620eff3146f7eaa19fe0cc0e
Patrick <15920996910sun@gmail.com>
2017-01-16 22:47:30 +0800

    更新上传地址

--- index.js
+++ index.js
@@ -8,30 +8,31 @@ var wilddogUrl = "https://android-libs.wilddogio.com/libs/"
 var jsonSuffix = ".json"
     // var target = "https://github.com/XXApple/AndroidLibs/blob/master/%E5%88%97%E8%A1%A8List/README.md"
 
-var masterKey = 'UZ4siGMJU177OqktfwSYxUJ9R3sJDSFVI475nCF9'
+var count = 0
 
-scan("https://github.com/XXApple/AndroidLibs/blob/master/%E5%88%97%E8%A1%A8List/README.md", "list")
-scan("https://github.com/XXApple/AndroidLibs/blob/master/%E5%8A%A8%E7%94%BBAnimation/README.md", "animation")
-scan("https://github.com/XXApple/AndroidLibs/blob/master/%E5%9B%BE%E6%A0%87Icon/README.md", "icon")
-scan("https://github.com/XXApple/AndroidLibs/blob/master/%E5%9B%BE%E7%89%87%E6%A1%86%E6%9E%B6Image/README.md", "image")
-scan("https://github.com/XXApple/AndroidLibs/tree/master/%E5%9B%BE%E8%A1%A8Chart/README.md", "chart")
-scan("https://github.com/XXApple/AndroidLibs/tree/master/%E5%AE%8C%E6%95%B4%E5%BC%80%E6%BA%90%E9%A1%B9%E7%9B%AEProject/README.md", "project")
-scan("https://github.com/XXApple/AndroidLibs/tree/master/%E5%B8%83%E5%B1%80Layout/README.md", "layout")
-scan("https://github.com/XXApple/AndroidLibs/tree/master/%E5%BC%80%E5%8F%91%E6%A1%86%E6%9E%B6Framework/README.md", "framework")
-scan("https://github.com/XXApple/AndroidLibs/tree/master/%E5%BC%B9%E6%A1%86Dialog/README.md", "dialog")
-scan("https://github.com/XXApple/AndroidLibs/tree/master/%E6%8C%89%E9%92%AEButton/README.md", "button")
-scan("https://github.com/XXApple/AndroidLibs/tree/master/%E6%96%87%E6%9C%ACLabel/README.md", "label")
-scan("https://github.com/XXApple/AndroidLibs/tree/master/%E7%89%B9%E6%95%88Effect/README.md", "effect")
-scan("https://github.com/XXApple/AndroidLibs/tree/master/%E7%BD%91%E7%BB%9C%E6%A1%86%E6%9E%B6Network/README.md", "network")
-scan("https://github.com/XXApple/AndroidLibs/tree/master/%E8%87%AA%E5%AE%9A%E4%B9%89%E6%8E%A7%E4%BB%B6Custom/README.md", "custom")
-scan("https://github.com/XXApple/AndroidLibs/tree/master/%E8%8F%9C%E5%8D%95Menu/README.md", "menu")
+// scan("https://github.com/XXApple/AndroidLibs/blob/master/%E5%88%97%E8%A1%A8List/README.md", "list")
+// scan("https://github.com/XXApple/AndroidLibs/blob/master/%E5%8A%A8%E7%94%BBAnimation/README.md", "animation")
+// scan("https://github.com/XXApple/AndroidLibs/blob/master/%E5%9B%BE%E6%A0%87Icon/README.md", "icon")
+// scan("https://github.com/XXApple/AndroidLibs/blob/master/%E5%9B%BE%E7%89%87%E6%A1%86%E6%9E%B6Image/README.md", "image")
+// scan("https://github.com/XXApple/AndroidLibs/tree/master/%E5%9B%BE%E8%A1%A8Chart/README.md", "chart")
+// scan("https://github.com/XXApple/AndroidLibs/tree/master/%E5%AE%8C%E6%95%B4%E5%BC%80%E6%BA%90%E9%A1%B9%E7%9B%AEProject/README.md", "project")
+// scan("https://github.com/XXApple/AndroidLibs/tree/master/%E5%B8%83%E5%B1%80Layout/README.md", "layout")
+// scan("https://github.com/XXApple/AndroidLibs/tree/master/%E5%BC%80%E5%8F%91%E6%A1%86%E6%9E%B6Framework/README.md", "framework")
+// scan("https://github.com/XXApple/AndroidLibs/tree/master/%E5%BC%B9%E6%A1%86Dialog/README.md", "dialog")
+// scan("https://github.com/XXApple/AndroidLibs/tree/master/%E6%8C%89%E9%92%AEButton/README.md", "button")
+// scan("https://github.com/XXApple/AndroidLibs/tree/master/%E6%96%87%E6%9C%ACLabel/README.md", "label")
+// scan("https://github.com/XXApple/AndroidLibs/tree/master/%E7%89%B9%E6%95%88Effect/README.md", "effect")
+// scan("https://github.com/XXApple/AndroidLibs/tree/master/%E7%BD%91%E7%BB%9C%E6%A1%86%E6%9E%B6Network/README.md", "network")
+// scan("https://github.com/XXApple/AndroidLibs/tree/master/%E8%87%AA%E5%AE%9A%E4%B9%89%E6%8E%A7%E4%BB%B6Custom/README.md", "custom")
+// scan("https://github.com/XXApple/AndroidLibs/tree/master/%E8%8F%9C%E5%8D%95Menu/README.md", "menu")
 scan("https://github.com/XXApple/AndroidLibs/tree/master/%E8%BE%85%E5%8A%A9%E5%B7%A5%E5%85%B7%E7%B1%BBUtils/README.md", "utils")
-scan("https://github.com/XXApple/AndroidLibs/blob/master/%E8%BF%9B%E5%BA%A6%E6%9D%A1Progressbar/README.md", "progressbar")
-
+// scan("https://github.com/XXApple/AndroidLibs/blob/master/%E8%BF%9B%E5%BA%A6%E6%9D%A1Progressbar/README.md", "progressbar")
+// scan("https://github.com/XXApple/AndroidLibs/blob/master/RxJava/README.md","rxjava")
 
 function scan(target, category) {
     request(target, function(err, response, body) {
-        console.log("Request" + target + " Complate ... ")
+        console.log("Request 【" + category + "】 Complate ... ")
+        console.log('scan count :'+ ++count)
         parse(body, category)
     })
 }
@@ -70,12 +71,11 @@ function parse(body, category) {
                 link: itemLink,
                 remark: itemRemark,
                 category:category,
-                pic: itemPic
+                images: itemPic
             })
        
     })
     
-    // putWilddog(list, local)
 }
 
 function nextBlock(elememt) {
@@ -84,9 +84,10 @@ function nextBlock(elememt) {
 
 
 function putMongoDB(item){
+    // console.log(item)
     var options = {
-        method: 'PUT',
-        url: "http://localhost:3001/items/",
+        method: 'POST',
+        url: "http://localhost:8080/libs/",
         headers: {
             'content-type': 'application/json'
         },
@@ -97,29 +98,7 @@ function putMongoDB(item){
     request(options,function(err,response, body){
         if (err) throw new Error(err);
 
-        console.log(body)
+        // console.log(body)
     })
-
-}
-
-/**
- * 保存到野狗
- */
-function putWilddog(list, wilddogNode) {
-    var options = {
-        method: 'PUT',
-        url: wilddogUrl + wilddogNode + jsonSuffix+"?auth="+masterKey,
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: list,
-        json: true
-    };
-
-    request(options, function(error, response, body) {
-        if (error) throw new Error(error);
-
-        console.log("put Wilddog +" + wilddogNode + " Complate");
-    });
 
 }
